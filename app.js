3572540c66a352aea99e2a7d357f28d345325e27
cheneyweb <457299596@qq.com>
2017-06-11 22:40:46 +0800

    koa-xmodel 1.0

--- app.js
+++ app.js
@@ -8,19 +8,10 @@ const bodyParser = require('koa-bodyparser')
 const mount = require('koa-mount')
 const xmodel = require(__dirname + '/xmodel_modules/koa-xmodel/index.js')
 // 持久层相关
-const fs = require('fs')
-const sequelize = require(__dirname + '/src/sequelize/sequelize.js')
 let modelDir = __dirname + config.get('server').modelDir
+const sequelize = require(__dirname + '/src/sequelize/sequelize.js')
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.get('log').level })
-
-// 首先同步所有实体和数据库
-fs.readdirSync(modelDir).forEach(function (filename) {
-	require(modelDir + filename)
-})
-sequelize.sync().then(function () {
-	log.info('xmodel所有实体已同步数据库')
-})
 
 // 初始化应用服务
 const app = new Koa()
@@ -28,7 +19,7 @@ const app = new Koa()
 app.use(bodyParser())
 
 // 引入koa-xmodel中间件
-xmodel.modelDir = modelDir
+xmodel.initConnect(modelDir,sequelize)
 app.use(mount(controllerRoot, xmodel.routes()))
 
 // 启动应用服务
--- xmodel_modules/koa-xmodel/index.js
+++ xmodel_modules/koa-xmodel/index.js
@@ -5,6 +5,22 @@ const router = new Router()
 // 控制器加载
 const CONTROLLER_PATH = __dirname + '/controller/ModelController.js'
 const MODEL_SUFFIX = '.js'
+const fs = require('fs')
+// 日志
+const log = require('tracer').colorConsole()
+
+/**
+ * 同步所有实体和数据库
+ */
+router.initConnect = function (modelDir,sequelize) {
+    router.modelDir = modelDir
+    fs.readdirSync(modelDir).forEach(function (filename) {
+        require(modelDir + filename)
+    })
+    sequelize.sync().then(function () {
+        log.info('xmodel所有实体已同步数据库')
+    })
+}
 
 // 配置路由与Controller方法的绑定
 /**
@@ -39,12 +55,12 @@ router.post('/:model_name/update', async function (ctx, next) {
 /**
  * 复杂查询实体对象
  */
-router.post('/:model_name/query', async function(ctx, next) {
+router.post('/:model_name/query', async function (ctx, next) {
     // 从请求路径中获取Controller名称
     ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
     // 动态加载对应名称的方法
     let model = require(CONTROLLER_PATH)
-    if(router.modelDir){
+    if (router.modelDir) {
         model.modelDir = router.modelDir
     }
     let result = await model.query(ctx)
@@ -53,12 +69,12 @@ router.post('/:model_name/query', async function(ctx, next) {
 /**
  * 销毁实体对象
  */
-router.get('/:model_name/destroy/:id', async function(ctx, next) {
+router.get('/:model_name/destroy/:id', async function (ctx, next) {
     // 从请求路径中获取Controller名称
     ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
     // 动态加载对应名称的方法
     let model = require(CONTROLLER_PATH)
-    if(router.modelDir){
+    if (router.modelDir) {
         model.modelDir = router.modelDir
     }
     let result = await model.destroy(ctx)
@@ -67,12 +83,12 @@ router.get('/:model_name/destroy/:id', async function(ctx, next) {
 /**
  * 获取实体对象
  */
-router.get('/:model_name/get/:id', async function(ctx, next) {
+router.get('/:model_name/get/:id', async function (ctx, next) {
     // 从请求路径中获取Controller名称
     ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
     // 动态加载对应名称的方法
     let model = require(CONTROLLER_PATH)
-    if(router.modelDir){
+    if (router.modelDir) {
         model.modelDir = router.modelDir
     }
     let result = await model.get(ctx)
