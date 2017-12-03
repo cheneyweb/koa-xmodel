c704504bcae62db4b480f71fcd560b6cf5f0b5b1
cheneyweb <457299596@qq.com>
2017-12-03 23:13:00 +0800

    升级精简所有依赖，减少中转提升性能

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // 系统配置参数
 const config = require('config')
-const port = config.get('server').port
-const controllerRoot = config.get('server').controllerRoot
+const port = config.server.port
+const controllerRoot = config.server.controllerRoot
 // 应用服务相关
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const mount = require('koa-mount')
 const xmodel = require(__dirname + '/xmodel_modules/koa-xmodel/index.js')
 // 持久层相关
-let modelDir = __dirname + config.get('server').modelDir
+let modelDir = __dirname + config.server.modelDir
 const sequelize = require(__dirname + '/src/sequelize/sequelize.js')
 // 日志相关
-const log = require('tracer').colorConsole({ level: config.get('log').level })
+const log = require('tracer').colorConsole({ level: config.log.level })
 
 // 初始化应用服务
 const app = new Koa()
@@ -19,15 +19,15 @@ const app = new Koa()
 app.use(bodyParser())
 
 // 引入koa-xmodel中间件
-xmodel.initConnect(modelDir,sequelize)
+xmodel.initConnect(modelDir, sequelize)
 app.use(mount(controllerRoot, xmodel.routes()))
 
 // 启动应用服务
 app.listen(port)
 
-log.info(`XModel服务已启动,执行环境:${process.env.NODE_ENV},端口:${port}...`)
-log.info(`[POST]http://host:${port}/xmodel/MODEL/create`)
-log.info(`[POST]http://host:${port}/xmodel/MODEL/update`)
-log.info(`[POST]http://host:${port}/xmodel/MODEL/query`)
-log.info(`[GET]http://host:${port}/xmodel/MODEL/get/:id`)
-log.info(`[GET]http://host:${port}/xmodel/MODEL/destroy/:id`)
+log.info(`XModel服务启动【执行环境:${process.env.NODE_ENV},端口:${port}】`)
+log.info(`[POST]http://localhost:${port}/xmodel/MODEL/create`)
+log.info(`[POST]http://localhost:${port}/xmodel/MODEL/update`)
+log.info(`[POST]http://localhost:${port}/xmodel/MODEL/query`)
+log.info(`[GET ]http://localhost:${port}/xmodel/MODEL/get/:id`)
+log.info(`[GET ]http://localhost:${port}/xmodel/MODEL/destroy/:id`)
--- xmodel_modules/koa-xmodel/index.js
+++ xmodel_modules/koa-xmodel/index.js
@@ -3,7 +3,7 @@ const Router = require('koa-router')
 // 初始化路由
 const router = new Router()
 // 控制器加载
-const CONTROLLER_PATH = __dirname + '/controller/ModelController.js'
+const modelController = require(__dirname + '/controller/ModelController.js')
 const MODEL_SUFFIX = '.js'
 const fs = require('fs')
 // 日志
@@ -12,7 +12,7 @@ const log = require('tracer').colorConsole()
 /**
  * 同步所有实体和数据库
  */
-router.initConnect = function (modelDir,sequelize) {
+router.initConnect = function (modelDir, sequelize) {
     router.modelDir = modelDir
     fs.readdirSync(modelDir).forEach(function (filename) {
         require(modelDir + filename)
@@ -27,72 +27,82 @@ router.initConnect = function (modelDir,sequelize) {
  * 创建实体对象
  */
 router.post('/:model_name/create', async function (ctx, next) {
-    // 从请求路径中获取Controller名称
-    ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
-    // 动态加载对应名称的方法
-    let model = require(CONTROLLER_PATH)
-    if (router.modelDir) {
-        model.modelDir = router.modelDir
+    try {
+        // 从请求路径中获取Controller名称
+        ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
+        // 动态加载对应名称的方法
+        if (router.modelDir) { modelController.modelDir = router.modelDir }
+        let result = await modelController.create(ctx)
+        ctx.body = result
+    } catch (error) {
+        log.error(error)
+        ctx.body = '路由服务异常'
     }
-    let result = await model.create(ctx)
-    ctx.body = result
 })
 
 /**
  * 更新实体对象
  */
 router.post('/:model_name/update', async function (ctx, next) {
-    // 从请求路径中获取Controller名称
-    ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
-    // 动态加载对应名称的方法
-    let model = require(CONTROLLER_PATH)
-    if (router.modelDir) {
-        model.modelDir = router.modelDir
+    try {
+        // 从请求路径中获取Controller名称
+        ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
+        // 动态加载对应名称的方法
+        if (router.modelDir) { modelController.modelDir = router.modelDir }
+        let result = await modelController.update(ctx)
+        ctx.body = result
+    } catch (error) {
+        log.error(error)
+        ctx.body = '路由服务异常'
     }
-    let result = await model.update(ctx)
-    ctx.body = result
 })
 /**
  * 复杂查询实体对象
  */
 router.post('/:model_name/query', async function (ctx, next) {
-    // 从请求路径中获取Controller名称
-    ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
-    // 动态加载对应名称的方法
-    let model = require(CONTROLLER_PATH)
-    if (router.modelDir) {
-        model.modelDir = router.modelDir
+    try {
+        // 从请求路径中获取Controller名称
+        ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
+        // 动态加载对应名称的方法
+        if (router.modelDir) { modelController.modelDir = router.modelDir }
+        let result = await modelController.query(ctx)
+        ctx.body = result
+    } catch (error) {
+        log.error(error)
+        ctx.body = '路由服务异常'
     }
-    let result = await model.query(ctx)
-    ctx.body = result
 })
 /**
  * 销毁实体对象
  */
 router.get('/:model_name/destroy/:id', async function (ctx, next) {
-    // 从请求路径中获取Controller名称
-    ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
-    // 动态加载对应名称的方法
-    let model = require(CONTROLLER_PATH)
-    if (router.modelDir) {
-        model.modelDir = router.modelDir
+    try {
+        // 从请求路径中获取Controller名称
+        ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
+        // 动态加载对应名称的方法
+        if (router.modelDir) { modelController.modelDir = router.modelDir }
+        let result = await modelController.destroy(ctx)
+        ctx.body = result
+    } catch (error) {
+        log.error(error)
+        ctx.body = '路由服务异常'
     }
-    let result = await model.destroy(ctx)
-    ctx.body = result
 })
 /**
  * 获取实体对象
  */
 router.get('/:model_name/get/:id', async function (ctx, next) {
-    // 从请求路径中获取Controller名称
-    ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
-    // 动态加载对应名称的方法
-    let model = require(CONTROLLER_PATH)
-    if (router.modelDir) {
-        model.modelDir = router.modelDir
+    try {
+        // 从请求路径中获取Controller名称
+        ctx.request.modelName = transJavaStyle(ucfirst(ctx.params.model_name)) + MODEL_SUFFIX
+        // 动态加载对应名称的方法
+        if (router.modelDir) { modelController.modelDir = router.modelDir }
+        let result = await modelController.get(ctx)
+        ctx.body = result
+    } catch (error) {
+        log.error(error)
+        ctx.body = '路由服务异常'
     }
-    let result = await model.get(ctx)
-    ctx.body = result
 })
 
 function ucfirst(str) {
